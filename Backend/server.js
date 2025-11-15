8c5978020e281ed59cf0a5cfcb8e5bc40af2ba23
Tarmanjeet <tarmanjeetkaur@gmail.com>
2025-11-15 06:15:33 +0530

    Cors added

--- Backend/server.js
+++ Backend/server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://funny-beignet-82edb3.netlify.app/"
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
