417d982841b25227263e1e021fe9ce1f4cb4fb21
Tarmanjeet <tarmanjeetkaur@gmail.com>
2025-11-15 06:07:39 +0530

    Cors added

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://funny-beignet-82edb3.netlify.app/"
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected successfully'))
